db4e92fddfe158299e0dec22f91b6db92e7815e3
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-05 19:50:31 +0200

    Created basic structure. Created context.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
 function App() {
-  return <h2>Megamenu</h2>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
 }
 
 export default App;
