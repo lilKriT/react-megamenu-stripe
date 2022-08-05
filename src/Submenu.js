ab3d80a2e0fbe16fab32344963bd2f9cc763a35c
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-05 21:26:40 +0200

    Displaying submenu links.

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,18 +2,43 @@ import React, { useState, useEffect, useRef } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
   const container = useRef();
+  const [columns, setColumns] = useState("col-2");
+
   useEffect(() => {
+    setColumns("col-2");
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length === 4) {
+      setColumns("col-4");
+    }
+  }, [location, links]);
 
   return (
     <aside className={`submenu ${isSubmenuOpen && "show"}`} ref={container}>
-      Submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
